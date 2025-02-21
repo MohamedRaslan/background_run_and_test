d02b6b743055588be6a466256a4b8c3510727719
MohamedRaslan <mohamedraslang@gmail.com>
2025-02-21 11:17:07 +0200

    chore: Enhance GitHub release comments and remove outdated dependencies

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,6 +10,18 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer", // Analyzes commits to determine the version bump
     "@semantic-release/release-notes-generator", // Generates release notes
-    "@semantic-release/github", // Publishes the release to GitHub
+    [
+      "@semantic-release/github", // Publishes the release to GitHub
+      {
+        successComment:
+          ":tada: This ${issue.pull_request ? 'pull request' : 'issue'} has been resolved in version ${nextRelease.version} :tada:\n\nThe release is available on [GitHub release](<github_release_url>)",
+        failComment:
+          "🚨 This release from branch ${branch.name} had failed due to the following errors:\n- ${errors.map(err => err.message).join('\\n- ')}",
+        failTitle: "🚨 Automated release failed for ${nextRelease.version} release",
+        labels: ["auto-release-fail"],
+        releasedLabels: ['released<%= nextRelease.channel ? ` on @${nextRelease.channel}` : "production" %>'],
+      },
+    ],
   ],
 };
+
