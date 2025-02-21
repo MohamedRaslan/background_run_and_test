b3950e3450b9803acbc1220bd7947d37e92f6e79
MohamedRaslan <mohamedraslang@gmail.com>
2025-02-21 11:29:46 +0200

    chore(deps): update globals and typescript-eslint dependencies

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,27 +1,29 @@
 module.exports = {
   branches: [
-    "main", // Regular releases for the main branch
+    'main', // Regular releases for the main branch
     {
-      name: "staging", // Pre-releases for the staging branch
-      channel: "beta",
-      prerelease: "beta",
-    },
+      name: 'staging', // Pre-releases for the staging branch
+      channel: 'beta',
+      prerelease: 'beta'
+    }
   ],
   plugins: [
-    "@semantic-release/commit-analyzer", // Analyzes commits to determine the version bump
-    "@semantic-release/release-notes-generator", // Generates release notes
+    '@semantic-release/commit-analyzer', // Analyzes commits to determine the version bump
+    '@semantic-release/release-notes-generator', // Generates release notes
     [
-      "@semantic-release/github", // Publishes the release to GitHub
+      '@semantic-release/github', // Publishes the release to GitHub
       {
         successComment:
           ":tada: This ${issue.pull_request ? 'pull request' : 'issue'} has been resolved in version ${nextRelease.version} :tada:\n\nThe release is available on [GitHub release](<github_release_url>)",
         failComment:
           "🚨 This release from branch ${branch.name} had failed due to the following errors:\n- ${errors.map(err => err.message).join('\\n- ')}",
-        failTitle: "🚨 Automated release failed for ${nextRelease.version} release",
-        labels: ["auto-release-fail"],
-        releasedLabels: ['released<%= nextRelease.channel ? ` on @${nextRelease.channel}` : "production" %>'],
-      },
-    ],
-  ],
-};
-
+        failTitle:
+          '🚨 Automated release failed for ${nextRelease.version} release',
+        labels: ['auto-release-fail'],
+        releasedLabels: [
+          'released<%= nextRelease.channel ? ` on @${nextRelease.channel}` : "production" %>'
+        ]
+      }
+    ]
+  ]
+}
